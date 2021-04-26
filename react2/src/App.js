a7246da936ff37f57095201d3f293912db1c0b56
TliliFadhila <ziiwii40@gmail.com>
2021-04-26 22:50:39 +0100

    changes

--- react2/src/App.js
+++ react2/src/App.js
@@ -1,6 +1,5 @@
-/* eslint-disable jsx-a11y/alt-text */
-/* eslint-disable react/jsx-no-undef */
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react'
+import { ReactVideo } from "reactjs-media";
 import logo from './logo.svg';
 import './App.css';
 import './Style.css';
@@ -9,15 +8,17 @@ function App() {
   return (
     <div className="App">
       <div style={{border:'solid 1px black' , maxWidth: '100vw'}}>
-        <h1 className="title red">Your name here</h1>
-        <img src={logo}/>
-        <img src="./logo192"/>
+        <h1 className="title red">Your Name Here</h1>
+        <img alt="Logo" src={logo}/>
+        <img alt="Logo" src="/logo512.png" />
       </div>
-      <YoutubePlayer
-                src="https://youtu.be/UZCO5k1Nu70" // Reqiured
-                width={650}
-                height={600}
-      />
+      <div>
+            <ReactVideo
+                src="https://www.youtube.com/watch?v=ZH9wNQlnukY"
+                poster="https://www.example.com/poster.png"
+                primaryColor="red"
+            />
+        </div>
     </div>
 );
 }
