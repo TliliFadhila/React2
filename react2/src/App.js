99b8d94b36c691738da52d4258957c0a8e0e6d58
TliliFadhila <ziiwii40@gmail.com>
2021-04-26 21:25:54 +0100

    changes

--- react2/src/App.js
+++ react2/src/App.js
@@ -1,12 +1,13 @@
 /* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable react/jsx-no-undef */
+import 'bootstrap/dist/css/bootstrap.min.css';
 import logo from './logo.svg';
 import './App.css';
 import './Style.css';
 
 function App() {
   return (
-    <>
+    <div className="App">
       <div style={{border:'solid 1px black' , maxWidth: '100vw'}}>
         <h1 className="title red">Your name here</h1>
         <img src={logo}/>
@@ -17,8 +18,8 @@ function App() {
                 width={650}
                 height={600}
       />
-    </>
-  );
+    </div>
+);
 }
 
 export default App;
